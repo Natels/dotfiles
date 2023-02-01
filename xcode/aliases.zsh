35ea2b9dc2791f17ae04f7e478cb845c54063f02
Nate Smith <nate.smith@betsecure.com>
2023-02-01 11:13:44 +1000

    Also check for `Package.swift`, when opening Xcode

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,3 +1,3 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-alias x='exists() { [[ -d "$1" ]] }; if exists *.(xcworkspace|playground|xcodeproj); then open -a Xcode.app *.(xcworkspace|playground|xcodeproj); fi'
+alias x='exists() { [[ -d "$1" ]] }; if [[ -a Package.swift ]]; then open -a Xcode.app .; elif exists *.(xcworkspace|playground|xcodeproj); then open -a Xcode.app *.(xcworkspace|playground|xcodeproj); fi'
 
