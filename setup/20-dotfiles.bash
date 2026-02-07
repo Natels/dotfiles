8f7918dff697bb917111044a51ebf55255b6b3b5
Nate Smith <natoraider@gmail.com>
2026-02-07 15:57:39 +1000

    Stop overwriting ~/.ssh

--- setup/20-dotfiles.bash
+++ setup/20-dotfiles.bash
@@ -9,6 +9,19 @@ for FILE in "$HOME/dotfiles/dotfiles/".* "$HOME/dotfiles/dotfiles/"*; do
     continue
   fi
 
+  if [[ -d "$FILE" && "$NAME" == ".ssh" ]]; then
+    echo "Merging $NAME..."
+    mkdir -p "$HOME/.ssh"
+    chmod 700 "$HOME/.ssh"
+    for SSH_FILE in "$FILE"/*; do
+      SSH_NAME=$(basename "$SSH_FILE")
+      if [[ ! -L "$HOME/.ssh/$SSH_NAME" ]]; then
+        ln -s "$SSH_FILE" "$HOME/.ssh/$SSH_NAME"
+      fi
+    done
+    continue
+  fi
+
   if [[ -L "$HOME/$NAME" ]]; then
     echo "$NAME is already a symlink."
     continue
